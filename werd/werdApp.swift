e4fce4b617be334dc1243dbf021dcc3b2d66647d
wedding <azizah.alshami@hotmail.com>
2022-11-09 12:38:34 +0300

    Initial Commit

---
+++ werd/werdApp.swift
@@ -0,0 +1,17 @@
+//
+//  werdApp.swift
+//  werd
+//
+//  Created by azizah ahmed alshami on 15/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct werdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
